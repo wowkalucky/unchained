ea252a27bfb4f7e15fa5a265177a185b17240fc2
wowkalucky <wowkalucky@gmail.com>
2013-02-17 17:19:03 +0200

    Configure urls file

--- project/urls.py
+++ project/urls.py
@@ -4,14 +4,14 @@ from django.conf.urls import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^project/', include('project.foo.urls')),
+urlpatterns = \
+    patterns('',
+             url(r'^$', 'project.views.index', name='index'),
+             )
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+# url(r'^project/', include('project.foo.urls')),
+# Uncomment the admin/doc line below to enable admin documentation:
+# url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-)
+# Uncomment the next line to enable the admin:
+# url(r'^admin/', include(admin.site.urls)),
