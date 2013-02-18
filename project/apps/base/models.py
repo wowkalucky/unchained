f6ce7c7a28538d1624e6646490ad18a2d59b2b48
wowkalucky <wowkalucky@gmail.com>
2013-02-18 17:51:43 +0200

    Create base app models: Pupil, ClassId

--- project/apps/base/models.py
+++ project/apps/base/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Pupil(models.Model):
+
+    first_name = models.CharField('first name', max_length=30)
+    middle_name = models.CharField('middle name', max_length=30)
+    last_name = models.CharField('last name', max_length=30)
+    birth_date = models.DateField()
+    pupil_id = models.IntegerField()
+
+    def get_classId_set(self):
+        return self.classid_set.all()
+
+    def full_name(self):
+        return u"%s %s" % (self.last_name, self.first_name)
+
+    class Meta:
+        verbose_name = ('Pupil')
+        verbose_name_plural = ('Pupils')
+
+    def __unicode__(self):
+        return self.full_name()
+
+
+class ClassId(models.Model):
+
+    class_id = models.CharField("class", max_length=5)
+    praepostor = models.ForeignKey(Pupil)
+
+    class Meta:
+        verbose_name = ('ClassId')
+        verbose_name_plural = ('ClassIds')
+
+    def __unicode__(self):
+        return self.class_id
